b9167b6ebbfb23e044a5ecb00c9e01d00a407ebe
Steven Nguyen <142837626+Baconano@users.noreply.github.com>
2026-04-25 14:08:17 -0500

    bro push it

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from stego_logic import embed_message, extract_message
 
 
-app = Flask(__name__, template_folder='Templates')
+app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/users.db'
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
